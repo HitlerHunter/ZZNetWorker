7173dfbd9c23fc34b23e523b75d714022768d999
zenglizhi <zenglizhi@zenglizhideiMac.local>
2018-08-27 09:11:51 +0800

    s

---
+++ ZZNetWorker/Classes/ZZNetWorkHandler.h
@@ -0,0 +1,13 @@
+//
+//  ZZNetWorkHandler.h
+//  AFNetworking
+//
+//  Created by zenglizhi on 2018/8/25.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZZNetWorkHandler : NSObject
+
++ (NSDictionary *)jsonFromResponseObject:(id)responseObject;
+@end
